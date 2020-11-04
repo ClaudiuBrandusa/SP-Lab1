f2eb35b04f6fde063f9015e5bceb1fed651a5af7
ClaudiuBrandusa <claudiu.office99@gmail.com>
2020-11-04 12:13:58 +0200

    lab 4

--- src/main/Image.java
+++ src/main/Image.java
@@ -1,9 +1,16 @@
 package main;
 
+import java.util.concurrent.TimeUnit;
+
 public class Image implements Element {
     String image;
     Image(String image) {
         this.image = image;
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
